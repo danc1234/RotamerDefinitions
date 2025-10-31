9868a35badf2c8e1fad9930a336fa446ba04f454
danc1234 <144494468+danc1234@users.noreply.github.com>
2025-10-31 11:38:59 -0400

    added Points class

---
+++ Points.py
@@ -0,0 +1,33 @@
+import math
+
+class Points:
+    def __init__(self, x, y, z): # x, y, and z are coordinates of a point, they are floats
+        self.x = x
+        self.y = y
+        self.z = z
+
+    # Accessor Methods
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y 
+    def getZ(self):
+        return self.z
+
+    # self is the instance (the thing you attach .distance() to) and point is the other point.
+    # This method returns a float.
+    def distance(self, point):
+        a = (self.x-point.getX())**2
+        b = (self.y-point.getY())**2
+        c = (self.z-point.getZ())**2
+        return math.sqrt(a+b+c)
+
+    # Parameters are same as distance, except this time, vector returns a list
+    def vector(self, point):
+        return [self.x-point.getX(), self.y-point.getY(), self.z-point.getZ()]
+
+
+if __name__ == "__main__":
+    borb = Points(0,0,0)
+    barb = Points(0,3,4)
+    print(barb.distance(borb))    
